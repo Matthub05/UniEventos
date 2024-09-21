987248b0009761afc5efe6b117057bcdafd9167c
Zark_D <vargascardona12@gmail.com>
2024-09-21 15:12:13 -0500

    Add password recovery screen

--- app/src/main/java/com/example/unieventos/ui/screens/ForgotPassword.kt
+++ app/src/main/java/com/example/unieventos/ui/screens/ForgotPassword.kt
@@ -1,34 +1,90 @@
 package com.example.unieventos.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.example.unieventos.R
 
-@Composable
-fun ForgotPasswordScreen(){
-    Scaffold { paddingValues ->
-        ForgotPasswordForm(paddingValues)
-    }
-}
+import com.example.unieventos.ui.components.TextFieldForm
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ForgotPasswordForm(paddingValues: PaddingValues) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = ":)",
-            style = MaterialTheme.typography.headlineMedium
-        )
+fun ForgotPasswordScreen() {
+    var codigo by rememberSaveable { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            LargeTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                ),
+                title = {
+                    Text(
+                        text= stringResource(id = R.string.titulo_contrasenia),
+                        fontWeight= FontWeight.ExtraBold,
+                        fontSize = 30.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+
+            )
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextFieldForm(
+                value = codigo,
+                onValueChange = { codigo = it },
+                supportingText = "La cédula no es válida",
+                label = "Cédula",
+                onValidate = { it.isEmpty() || it.toIntOrNull() == null },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+
+
+        }
+
     }
 }
